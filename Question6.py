d3fe537257a96254b60a1d23b517375a869ef1e7
JUSTASTUDENT123 <68987134+JUSTASTUDENT123@users.noreply.github.com>
2024-06-05 21:52:34 +0530

    Update Question6.py

--- Question6.py
+++ Question6.py
@@ -1,3 +1,4 @@
+#Question 6
 ones=int(input("Enter ones digit of the 3 digit number: "))
 tens = int(input("Enter tens digit of the 3 digit number"))
 hundreds = int(input("Enter hundreds digit of the 3 digit number"))
