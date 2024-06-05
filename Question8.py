34b96f06bc59575f5d0e2ab917e9136ad154811c
JUSTASTUDENT123 <68987134+JUSTASTUDENT123@users.noreply.github.com>
2024-06-05 21:51:47 +0530

    Update Question8.py

--- Question8.py
+++ Question8.py
@@ -1,3 +1,4 @@
+#Question 8
 num1=int(input("Enter first number: "))
 num2=int(input("Enter second number: "))
 print("Enter + for Addition, - for subtraction, * for multiplication and / for division")
